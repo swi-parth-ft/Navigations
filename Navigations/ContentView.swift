588cd89d81aab5ac3e0e67dc0f7bd8125a6181b5
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-10 10:35:25 -0400

    Return to root view programmatically 1.1

--- Navigations/ContentView.swift
+++ Navigations/ContentView.swift
@@ -19,14 +19,16 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            
             NavigationStack(path: $path) {
-                DetailView(number: 0, path: $path)
-                    .navigationDestination(for: Int.self) { i in
-                        DetailView(number: i, path: $path)
-                    }
+                VStack {
+                    NavigationLink("Start", value: 0)
+                }
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
+                }
             }
         }
+        
     }
 }
 
@@ -34,13 +36,18 @@ struct DetailView: View {
     var number: Int
     @Binding var path: NavigationPath
     var body: some View {
-        NavigationLink("Go to random number", value: Int.random(in: 0...1000))
-            .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path = NavigationPath()
+        VStack {
+            NavigationLink("Go to random number", value: Int.random(in: 0...1000))
+                .navigationTitle("Number: \(number)")
+                .toolbar {
+                    Button("Home") {
+                        path = NavigationPath()
+                    }
                 }
-            }
+        }
+        .navigationDestination(for: Int.self) { i in
+            DetailView(number: i, path: $path)
+        }
     }
       
  
