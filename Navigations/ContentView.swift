6bbf0b8e6cd0db86ea6a32c2c71e3292650a1df7
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-10 11:10:08 -0400

    save NavigationStack paths using Codable

--- Navigations/ContentView.swift
+++ Navigations/ContentView.swift
@@ -13,40 +13,79 @@ struct Student: Hashable {
     var age: Int
 }
 
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "savedPath")
+    
+    init() {
+        
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representaion = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representaion)
+            try data.write(to: savePath)
+            
+        } catch {
+            print("failed to save navigation data")
+        }
+    }
+    
+    func home() {
+        path = NavigationPath()
+    }
+}
+
 struct ContentView: View {
     
-    @State private var path = NavigationPath()
+    @State private var pathStore = PathStore()
     
     var body: some View {
         VStack {
-            NavigationStack(path: $path) {
-                VStack {
-                    NavigationLink("Start", value: 0)
-                }
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $path)
-                }
-            }
+            NavigationStack(path: $pathStore.path) {
+                DetailView(number: 0)
+                            .navigationDestination(for: Int.self) { i in
+                                DetailView(number: i)
+                                    .toolbar {
+                                        Button("Home") {
+                                            pathStore.path = NavigationPath()
+                                        }
+                                    }
+                            }
+                            
+                    }
         }
         
     }
 }
 
 struct DetailView: View {
+    
+   
+
     var number: Int
-    @Binding var path: NavigationPath
     var body: some View {
         VStack {
             NavigationLink("Go to random number", value: Int.random(in: 0...1000))
                 .navigationTitle("Number: \(number)")
-                .toolbar {
-                    Button("Home") {
-                        path = NavigationPath()
-                    }
-                }
-        }
-        .navigationDestination(for: Int.self) { i in
-            DetailView(number: i, path: $path)
+                
+                
         }
     }
       
